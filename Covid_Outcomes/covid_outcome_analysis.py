ed12ccae8d492ce29086b68500345d12be4974f7
sgreysond <45319697+sgreysond@users.noreply.github.com>
2020-04-19 20:52:44 -0400

    Additional cleanup

--- Covid_Outcomes/covid_outcome_analysis.py
+++ Covid_Outcomes/covid_outcome_analysis.py
@@ -5,21 +5,15 @@ import pandas as pd
 import numpy as np
 import scipy.optimize as opt
 
-from typing import List, NamedTuple, Tuple, Sequence
+from typing import NamedTuple
 
 
 class CovidParams(NamedTuple):
     """Class to define and specify parameter values"""
 
-    time_horizon: int = 45  # Number of days allowed for cases to resolve
-    # constraint_weight_mult: float = 100.0  # Multiplier for constraint weight
+    time_horizon: int = 30  # Number of days allowed for cases to resolve
     cumulative_mode: bool = True  # Indicator of whether data is cumulative instead of incremental
-    initial_step_size: float = 10.0  # Initial step size for gradient descent method
-    step_decay_parameter: float = 0.000001  # Moderates how quickly step size declines. Larger means faster.
     epsilon: float = 1e-6  # Small number
-    beta_1: float = 0.995  # Expo weight for momentum in Adam optimizer
-    beta_2: float = 0.9995  # Expo weight for variance in Adam optimizer
-    max_iterations: int = 1e7  # Max number of iterations for the optimizer to run
 
 
 class CovidOutcomes:
@@ -174,99 +168,6 @@ class CovidOutcomes:
                         [0.9/self.params.epsilon]], np.float64).reshape([2 * num_data_rows_per_outcome + 1,])
 
         return rhs
-
-    def update_values(self, lagr_vars, x_vars, iteration):
-
-        step = self.params.initial_step_size * np.exp(-(iteration * self.params.step_decay_parameter))
-
-        # Adapt the Nadam optimizer algorithm to hone in on x variables.
-
-        # Calculation of derivatives WRT the Lagrangian function was adapted from
-        # https://stanford.edu/class/ee103/lectures/constrained-least-squares_slides.pdf
-        dl_dx = 2 * np.matmul(np.matmul(self.constrained_history_matrix.T, self.constrained_history_matrix), x_vars) - \
-                2 * np.matmul(self.constrained_history_matrix.T, self.right_hand_side) + \
-                (lagr_vars[1:]) * -1.0 * (x_vars < 0) - lagr_vars[0]
-
-        self.momentum_x = self.params.beta_1 * self.momentum_x + (1 - self.params.beta_1) * dl_dx
-        self.var_x = self.params.beta_2 * self.var_x + (1 - self.params.beta_2) * np.power(dl_dx, 2)
-
-        m_hat = self.momentum_x / (1 - np.power(self.params.beta_1, iteration)) + \
-                (1 - self.params.beta_1) * dl_dx / (1 - np.power(self.params.beta_1, iteration))
-        v_hat = self.var_x / (1 - np.power(self.params.beta_2, iteration))
-
-        x_vars = x_vars - step * m_hat / (np.sqrt(v_hat) + self.params.epsilon)
-
-        # Adapt the Nadam optimizer algorithm to hone in on Lagrange multipliers.
-        dl_dlagr = np.vstack([(sum(x_vars) - 1.0), x_vars * 1.0 * (x_vars < 0)])
-        mod_dl_dlagr = dl_dlagr.copy()
-        mod_dl_dlagr[1:] = dl_dlagr[1:] * (lagr_vars[1:] > self.params.epsilon)
-
-        self.momentum_l = self.params.beta_1 * self.momentum_l + (1 - self.params.beta_1) * dl_dlagr
-        self.var_l = self.params.beta_2 * self.var_l + (1 - self.params.beta_2) * np.power(dl_dlagr, 2)
-
-        m_hat = self.momentum_l / (1 - np.power(self.params.beta_1, iteration)) + \
-                (1 - self.params.beta_1) * dl_dlagr / (1 - np.power(self.params.beta_1, iteration))
-        v_hat = self.var_l / (1 - np.power(self.params.beta_2, iteration))
-
-        lagr_vars = lagr_vars - step * m_hat / (np.sqrt(v_hat) + self.params.epsilon)
-
-        # lagr_vars[1:] = lagr_vars[1:].clip(min=0)
-
-        grad_norm = np.linalg.norm(np.vstack([dl_dx, mod_dl_dlagr]))
-
-        return lagr_vars, x_vars, grad_norm
-
-    def constrained_regress(self):
-        """Use the Moore-Penrose pseudoinverse to find the constrained regression,
-        and build a DF for interpretability."""
-
-        raw_result = np.matmul(np.linalg.pinv(self.constrained_history_matrix), self.right_hand_side)
-
-        # First Lagrange variable corresponds to summation of probabilities to 1. Next 2*(time_horizon + 1)
-        # variables are for nonnegativity constraints.
-        lagr_vars = np.zeros(len(raw_result) + 1).reshape(len(raw_result) + 1, 1)
-
-        x_vars = raw_result
-
-        self.momentum_x = np.zeros(len(raw_result)).reshape(len(raw_result), 1)
-        self.momentum_l = np.zeros(len(raw_result) + 1).reshape(len(raw_result) + 1, 1)
-
-        self.var_x = np.zeros(len(raw_result)).reshape(len(raw_result), 1)
-        self.var_l = np.zeros(len(raw_result) + 1).reshape(len(raw_result) + 1, 1)
-
-        sum_lev = 0
-        min_lev = 0
-
-        grad_norm = 1.0
-        iteration = 1
-        while (iteration <= self.params.max_iterations) & (grad_norm >= self.params.epsilon):
-
-            lagr_vars, x_vars, grad_norm = self.update_values(lagr_vars, x_vars, iteration)
-
-            sum_lev = 0.999 * sum_lev + (1-0.999) * sum(x_vars)
-            min_lev = 0.999 * min_lev + (1-0.999) * min(x_vars)
-            if np.mod(iteration, 1e4) == 0:
-                print("Iterations " + str(iteration))
-                print("Sum of x variables: " + str(sum(x_vars)))
-                print("Minimum of x variables: " + str(min(x_vars)))
-                print("Smoothed sums: " + str(sum_lev))
-                print("Smoothed minima: " + str(min_lev))
-                print("Gradient norm is: " + str(grad_norm))
-
-            iteration += 1
-
-        print("Iterations applied: " + str(iteration))
-        print("Sum of x variables: " + str(sum(x_vars)))
-        print("Minimum of x variables: " + str(min(x_vars)))
-
-        result = pd.DataFrame({"days_from_diagnosis": list(range(self.params.time_horizon + 1)),
-                               "death_probability": x_vars[:self.params.time_horizon + 1],
-                               "recovery_probability": x_vars[self.params.time_horizon + 1:]})
-
-        print("Result is: ")
-        print(result)
-
-        return result
 
 
 if __name__ == "__main__":
